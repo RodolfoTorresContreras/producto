34c7163942e43b83102b380f38570fc3fdd1016a
Rodolfo Torres <rodotc9905@gmail.com>
2023-10-16 22:17:26 -0600

    20. Utilizando Ribbon para balanceo de carga en RestTemplate

--- src/main/java/com/formaciondbi/springboot/app/productos/productos/models/controllers/ProductoController.java
+++ src/main/java/com/formaciondbi/springboot/app/productos/productos/models/controllers/ProductoController.java
@@ -3,8 +3,11 @@ package com.formaciondbi.springboot.app.productos.productos.models.controllers;
 import com.formaciondbi.springboot.app.productos.productos.models.entity.Producto;
 import com.formaciondbi.springboot.app.productos.productos.models.services.IProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,21 +16,21 @@ import java.util.stream.Collectors;
 @RequestMapping("/producto")
 public class ProductoController {
 
-    @Autowired
-    private Environment environment;
+    @Value("${server.port}")
+    private Integer port;
 
     @Autowired
     private IProductoService iProductoService;
 
     @GetMapping
     public List<Producto> findAll(){
-        return iProductoService.findAll().stream().map(p -> p.port(Integer.parseInt(environment.getProperty("local.server.port")))).collect(Collectors.toList());
+        return iProductoService.findAll().stream().map(p -> p.port(port)).collect(Collectors.toList());
     }
 
 
     @GetMapping("/{id}")
     public Producto findById(@PathVariable("id") Long id){
-        return iProductoService.findById(id).port(Integer.parseInt(environment.getProperty("local.server.port")));
+        return iProductoService.findById(id).port(port);
     }
 
 }
